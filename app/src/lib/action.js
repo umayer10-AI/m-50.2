cecf6b02deb367394d6640d90310e35bed58679d
umayer10-AI <mdumayer10@gmail.com>
2026-05-07 20:43:24 +0600

    add

--- app/src/lib/action.js
+++ app/src/lib/action.js
@@ -9,12 +9,13 @@ export const createUser = async (formData) => {
     const res = await fetch(`http://localhost:5000/user`,{
         method: "POST",
         headers: {
-            
-        }
+            "content-type" : "application/json"
+        },
+        body: JSON.stringify(newUser)
     })
     const data = await res.json()
 
-    if(data.deletedCount > 0){
+    if(data.insertedId){
         revalidatePath('/user')
     }
 
